8f7041ecb7659d9ecd147beb43387c5d80ddffa2
Diego Motta <dmotta@farmakd.com>
2024-02-21 16:24:29 -0300

    Added features to form validation for applications

--- src/pages/security/roles/components/RoleList.jsx
+++ src/pages/security/roles/components/RoleList.jsx
@@ -113,7 +113,7 @@ const RoleList = () => {
     // Templates
     const header = (
         <div className="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
-            <h5 className="m-0"></h5>
+            <h5 className="m-0">&nbsp;</h5>
             <span className="block mt-2 md:mt-0 p-input-icon-left">
                 <i className="pi pi-search" />
                 <InputText type="search" onInput={(e) => onFilter(e)} placeholder="Buscar..." />
